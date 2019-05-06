3857928c7e468081189ca0ad7833bd5c3293777d
MadalinZen <42616393+MadalinZen@users.noreply.github.com>
2019-05-06 19:21:47 +0300

    Add files via upload

---
+++ src/main/java/com/sda/springbootexample/Components/CryptoData.java
@@ -0,0 +1,44 @@
+package com.sda.springbootexample.Components;
+
+import com.sda.springbootexample.Exception.UnknownRateException;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class CryptoData {
+
+    private Map<String,Double> cryptoMap = new HashMap() {
+
+        {
+
+            put("bitcoin",5286.26);
+            put("ethereum",169.23);
+            put("xrp",0.32);
+        }
+
+
+    };
+
+    /** Get the rate from internal map
+     *
+     * @param coinType
+     * @return
+     * @throws UnknownRateException
+     */
+
+    public double getRate(String coinType) throws UnknownRateException {
+
+        Double rate = cryptoMap.get(coinType) ;
+
+        if (rate == null) {
+
+            throw new UnknownRateException(cryptoMap.keySet());
+        }
+
+        return rate;
+            }
+
+
+}
